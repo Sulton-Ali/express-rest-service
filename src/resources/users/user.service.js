ba13a699031d180dfff94c9548aee9606096e131
JSultonali <jssalijalol@gmail.com>
2021-05-17 00:33:58 +0500

    feat: finish endpoint of users

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,18 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const createNewUser = (user) => usersRepo.createUser(user);
+
+const getUserById = (userId) => usersRepo.getUserById(userId);
+
+const updateUserById = (userData) => usersRepo.updateUserById(userData);
+
+const deleteUserById = (userId) => usersRepo.deleteUserById(userId);
+
+module.exports = {
+  getAll,
+  createNewUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+};
